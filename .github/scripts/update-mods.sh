eaba64c49fbeef8ad1fe27927b322dda9792f214
Lunar Starstrum <lunar@forsaken-borders.net>
2024-01-09 18:27:47 -0600

    Don't push duplicate updates when no mods have updated

--- .github/scripts/update-mods.sh
+++ .github/scripts/update-mods.sh
@@ -12,8 +12,10 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 
 # Check if there are any changes
+echo "updated=false" >> $GITHUB_OUTPUT
 git diff-index --quiet HEAD
 if [ "$?" == "1" ]; then
+  echo "updated=true" >> $GITHUB_OUTPUT
   git add . > /dev/null
   git commit -m "\`packwiz refresh\`." > /dev/null
   git push > /dev/null
